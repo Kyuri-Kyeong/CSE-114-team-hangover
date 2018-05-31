21cc1de149eab4b4b18df01a89055fe37fa110b2
Kyuri-Kyeong <36584357+Kyuri-Kyeong@users.noreply.github.com>
2018-05-31 17:49:37 +0900

    Add files via upload

---
+++ TransactionType.java
@@ -0,0 +1,24 @@
+//
+// Bank Account Manager
+// name: Kyuri Kyeong
+// Stonybrook ID: 111827215
+// CSE114 - Spring2018
+// HW 5
+//
+
+
+
+public enum TransactionType {
+	Deposit,
+	CheckCashed,
+	Interest,
+	PointOfSale,
+	Withdrawal,
+	buyMF,
+	buyStock,
+	sellMF,
+	
+	sellStock,
+	updateMFSharePrice
+	
+}
